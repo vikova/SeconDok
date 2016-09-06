8bece813389115d8c36c3a6a4ad4e70899a29af4
vikova <viktoriiakovalenko20@gmail.com>
2016-09-06 15:33:31 +0300

    experience was adde

--- main_module.rb
+++ main_module.rb
@@ -55,4 +55,68 @@ module MainModule
     @driver.find_element(css: '.go_next div').click
     sleep 5
   end
+
+  def second_dot
+    @wait.until { @driver.find_elements(css: '.block_cval') }
+    @driver.find_elements(:id,'reg_cval').each do |cval|
+      cval.click if cval.displayed?
+    end
+    @driver.find_elements(css: '.inp_search.inp_country input').each do |cat|
+      cat.send_keys 'второй' if cat.displayed?
+    end
+    sleep 3
+    @driver.find_elements(css: '[data-id="5"]').each do |qual|
+      qual.click if qual.displayed?
+
+    end
+    sleep 3
+
+    @driver.find_elements(css: '.block_educ').each do |educ|
+      educ.click if educ.displayed?
+    end
+    sleep 3
+    @driver.find_elements(css: '[data-id="220"]').each do |country|
+      country.click if country.displayed?
+    end
+    sleep 3
+    @driver.find_elements(css: '.inp_search.inp_country input').each do |search|
+      search.send_keys 'харьков' if search.displayed?
+    end
+    sleep 3
+    @driver.find_elements(:css, '.name').each do |city|
+      if city.displayed?
+        city.click
+        break
+      end
+    end
+    @driver.find_elements(css: '[data-id="2"]').each do |institute|
+      institute.click if institute.displayed?
+    end
+    @driver.find_elements(css: '[data-id="3"]').each do |degree|
+      degree.click if degree.displayed?
+    end
+    @wait.until { @driver.find_elements(css: '.btn.btn_white.btn_upload.ng-binding') }
+    @driver.find_elements(css: '.btn.btn_white.btn_upload.ng-binding').each do |button|
+      button.click if button.displayed?
+    end
+    sleep 5
+    @driver.find_elements(css: '[data-file="front"]').each do |front_photo|
+      front_photo.click if front_photo.displayed?
+    end
+    sleep 10
+    @driver.find_elements(css: '[data-file="back"]').each do |back_photo|
+      back_photo.click if back_photo.displayed?
+    end
+    sleep 10
+
+    @driver.find_elements(css: '.btn.btn_blue.btn_save_educ.ng-binding').each do |save_photo|
+      save_photo.click if save_photo.displayed?
+    end
+    sleep 5
+    @wait.until { @driver.find_elements(css: '.acts_block') }
+    @driver.find_elements(css: '.btn.btn_green.ng-binding').each do |next_button|
+      next_button.click if next_button.displayed?
+    end
+    sleep 5
+  end
 end
--- test_register.rb
+++ test_register.rb
@@ -17,69 +17,47 @@ class TestRegistration < Test::Unit::TestCase
     register
     rules
     first_dot
-    @wait.until { @driver.find_elements(css: '.block_cval') }
-    @driver.find_elements(:id,'reg_cval').each do |cval|
-      cval.click if cval.displayed?
-    end
-    @driver.find_elements(css: '.inp_search.inp_country input').each do |cat|
-      cat.send_keys 'второй' if cat.displayed?
+    second_dot
+    @driver.find_elements(:name, 'reg_tags').each do |spec|
+      spec.click if spec.displayed?
     end
     sleep 3
-    @driver.find_elements(css: '[data-id="5"]').each do |qual|
-      qual.click if qual.displayed?
-
+    @driver.find_elements(css: '[data-id="2"]').each do |spec1|
+      spec1.click if spec1.displayed?
+    end
+    @driver.find_elements(css: '.choose.choose_educt').each do |educt|
+      educt.click if educt.displayed?
+    end
+    @driver.find_elements(css: '.inp_search.inp_country input').each do |hospital|
+      hospital.send_keys 'второй' if hospital.displayed?
     end
     sleep 3
+    @driver.find_elements(css: '[data-id="5"]').each do |hospital1|
+      hospital1.click if hospital1.displayed?
+    end
+   # @driver.find_elements(css: '.inp_search.inp_country input').each do |year_start| year_start.send_keys '2010' if year_start.displayed? end
+    sleep 3
+    @driver.find_elements(css: '[data-id="2010"]').each do |year1|
+      year1.click if year1.displayed?
+    end
+   # @driver.find_elements(css: '.inp_search.inp_country input').each do |year_finish| year_finish.send_keys '2016' if year_finish.displayed? end
+    sleep 3
+    @driver.find_elements(css: '[data-id="2010"]').each do |year2|
+      year2.click if year2.displayed?
+    end
+   # @driver.find_elements(css: '.inp_search.inp_country input').each do |spec| spec.send_keys 'Нейрохирургия' if spec.displayed? end
+    sleep 3
+    @driver.find_elements(css: '[data-id="28"]').each do |spec1|
+      spec1.click if spec1.displayed?
+    end
 
-    @driver.find_elements(css: '.block_educ').each do |educ|
-      educ.click if educ.displayed?
-    end
-     sleep 3
-    @driver.find_elements(css: '[data-id="220"]').each do |country|
-      country.click if country.displayed?
-    end
-      sleep 3
-   @driver.find_elements(css: '.inp_search.inp_country input').each do |search|
-     search.send_keys 'харьков' if search.displayed?
-   end
-   sleep 3
-   @driver.find_elements(:css, '.name').each do |city|
-     if city.displayed?
-       city.click
-       break
-     end
-   end
-   @driver.find_elements(css: '[data-id="2"]').each do |institute|
-     institute.click if institute.displayed?
-   end
-   @driver.find_elements(css: '[data-id="3"]').each do |degree|
-     degree.click if degree.displayed?
-   end
-   @wait.until { @driver.find_elements(css: '.btn.btn_white.btn_upload.ng-binding') }
-    @driver.find_elements(css: '.btn.btn_white.btn_upload.ng-binding').each do |button|
-      button.click if button.displayed?
-    end
-    sleep 5
-    @driver.find_elements(css: '[data-file="front"]').each do |front_photo|
-      front_photo.click if front_photo.displayed?
-    end
-    sleep 10
-    @driver.find_elements(css: '[data-file="back"]').each do |back_photo|
-    back_photo.click if back_photo.displayed?
-    end
-    sleep 10
 
-    @driver.find_elements(css: '.btn.btn_blue.btn_save_educ.ng-binding').each do |save_photo|
-      save_photo.click if save_photo.displayed?
-    end
-    sleep 5
-    @wait.until { @driver.find_elements(css: '.acts_block') }
+
+    @wait.until { @driver.find_elements(css: '.go_next') }
     @driver.find_elements(css: '.btn.btn_green.ng-binding').each do |next_button|
-    next_button.click if next_button.displayed?
+      next_button.click if next_button.displayed?
     end
     sleep 5
-
-
 
 end
   def teardown         # close browser
